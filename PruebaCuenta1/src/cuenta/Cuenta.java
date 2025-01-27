792c4f2710142c27282cbe692eb4ab9ae87db1ca
JuanPa0109 <jusanchezre@unal.edu.co>
2025-01-26 23:04:43 -0500

    Add files via upload

---
+++ PruebaCuenta1/src/cuenta/Cuenta.java
@@ -0,0 +1,44 @@
+
+package cuenta;
+
+/**
+
+* Esta clase denominada Cuenta modela una cuenta bancaria con los
+* atributos saldo, número de consignaciones, número de retiros, tasa
+* anual de interés y comisión mensual.
+@version 1.2/2020
+*/
+public class Cuenta {
+     protected float saldo;
+     protected int númeroConsignaciones = 0;
+     protected int númeroRetiros = 0;
+     protected float tasaAnual;
+     protected float comisiónMensual = 0;
+     
+     public Cuenta(float saldo, float tasaAnual) {
+         this.saldo = saldo;
+         this.tasaAnual = tasaAnual;
+     }
+     
+     public void consignar(float cantidad) {
+         saldo = saldo + cantidad;
+         númeroConsignaciones = númeroConsignaciones + 1;
+     }
+     public void retirar(float cantidad) {
+         float nuevoSaldo = saldo - cantidad;
+         if (nuevoSaldo >= 0) {
+             saldo -= cantidad;
+             númeroRetiros = númeroRetiros + 1;
+         }else{System.out.println("La cantida a retirar excede el saldo actual.");
+     }
+     }
+     public void calcularInteres() {
+         float tasaMensual = tasaAnual / 12;
+         float interesMensual = saldo * tasaMensual;
+         saldo += interesMensual;
+     }
+     public void extractoMensual() {
+         saldo -= comisiónMensual;
+         calcularInteres();
+     }
+}
